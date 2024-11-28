52d5baf594de3dc842fd289158dd12b39beacd8a
曹轩 <caoxuan226@gmail.com>
2024-11-28 23:05:53 +0800

    整理文件，练习动画
    主要在matlab\6_DOF_formation\3-D+tilt_trirotor+Control\Airplane_sim_init_xuan；中进行测试-可视化动画设计-固定翼模式-三旋翼模式-切换-单机-编队

---
+++ 6_DOF_formation/3-D+tilt_trirotor+Control/Airplane_sim_init_xuan/runsim.m
@@ -0,0 +1,42 @@
+close all;
+
+clear;
+
+
+addpath('utils');
+addpath('traj');
+addpath('controller');
+addpath('test_tools');
+
+
+%% pre-calculated trajectories
+
+% trajhandle = @traj_line; % 可以运行的轨迹，与时间设定形式有关，配合simulation_3D ----- [F, M, att_des_save_A] = controlhandle(0, current_state_A, desired_state_A, params);  % s: [13 * 1]; 
+
+% trajhandle = @traj_circle; 
+trajhandle = @traj_helix; % 分析不同，设定单机轨迹，并拓展多机编队；
+% trajhandle = @traj_helix_adjust; % 还需调整
+
+%% Trajectory generation with waypoints
+%% You need to implement this
+% trajhandle = @traj_generator;
+% waypoints = [0    0   0;
+%              1    1   1;
+%              2    0   2;
+%              3    -1  1;
+%              4    0   0]';
+% trajhandle([],[],waypoints);
+
+%% controller selection
+% controlhandle = @controller_adrc_NL; % 需要调试
+% controlhandle = @controller_adrc;
+controlhandle = @controller_pid;
+
+
+% Run simulation with given trajectory generator and controller
+% state - n x 13, with each row having format [x, y, z, xdot, ydot, zdot, qw, qx, qy, qz, p, q, r]
+
+[t, state] = simulation_3d_att_plus_1_1_over(trajhandle, controlhandle); % 姿态跟踪完成版1_1，扩展接口实现
+% [t, state] = simulation_3d_att_plus(trajhandle, controlhandle); % 添加姿态跟踪效果
+
+
