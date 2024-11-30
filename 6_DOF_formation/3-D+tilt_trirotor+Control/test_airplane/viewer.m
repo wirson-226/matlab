f1af875a1d55a5c232fabcc8aa09e4133b296348
曹轩 <caoxuan226@gmail.com>
2024-11-30 18:15:32 +0800

    完成倾转，添加原点
    plot顺序逻辑描述已加

--- 6_DOF_formation/3-D+tilt_trirotor+Control/test_airplane/viewer.m
+++ 6_DOF_formation/3-D+tilt_trirotor+Control/test_airplane/viewer.m
@@ -1,12 +1,13 @@
-% 设置飞行器的位置和姿态
+%% 设置飞行器的位置和姿态
+% position = [10, 10, 10];    % 飞机位置
+% attitude = deg2rad([45, 45, 45]);    % 飞机姿态：roll pitch yaw, 弧度rad;rad = deg * pi / 180，正方向跟随坐标轴
+% tilt_angle = [90, 45];     % 电机倾转角 顺序为右，左，向下倾转为正 类pitch y轴正向；尾部固定。为c；
+
 position = [0, 0, 0];    % 飞机位置
-attitude = [0, 0, pi];    % 飞机姿态：roll pitch yaw, 弧度rad;rad = deg * pi / 180
-tilt_angle = [0, 0];     % 电机倾转角
-% wingspan = 15;              % 翼展 15 米
-% length = 20;                % 机身长度 20 米
-% tail_span = 5;              % 尾翼跨度 5 米
-% engine_pos = [0, 0, 10];    % 发动机位置
-% 
-% % 调用函数进行可视化
+attitude = deg2rad([0, 0, 0]);    % 飞机姿态：roll pitch yaw, 弧度rad;rad = deg * pi / 180，正方向跟随坐标轴
+tilt_angle = [0, 0];     % 电机倾转角 顺序为右，左，向下倾转为正 类pitch y轴正向；尾部固定。为c；
+
+%% 调用函数进行可视化
 % WingPlot(position, attitude, wingspan, length, tail_span, engine_pos);
-planeplot_ttr(position, attitude,tilt_angle);
+planeplot_ttr(position, attitude,tilt_angle); % 完成倾转，添加原点---
+% planeplot_ttr_test(position, attitude,tilt_angle);
